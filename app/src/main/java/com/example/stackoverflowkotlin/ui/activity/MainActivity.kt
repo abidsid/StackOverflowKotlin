d4f0baf7e8dec54be5375b2c82c389a913a25f07
abidsiddiqui <ajunaid@kent.edu>
2021-07-01 14:32:48 -0500

    Main Activity Setup

--- app/src/main/java/com/example/stackoverflowkotlin/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/stackoverflowkotlin/ui/activity/MainActivity.kt
@@ -1,14 +1,45 @@
 package com.example.stackoverflowkotlin.ui.activity
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.example.stackoverflowkotlin.R
+import com.example.stackoverflowkotlin.databinding.ActivityMainBinding
+import com.example.stackoverflowkotlin.ui.adapters.QuestionAdapter
+import com.example.stackoverflowkotlin.ui.viewModels.StackExchangeViewModel
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupTheme()
+    }
+
+    private fun setupTheme() {
+        val factory = ViewModelProvider.AndroidViewModelFactory.getInstance(application)
+        val viewModel = ViewModelProvider(this, factory).get(StackExchangeViewModel::class.java)
+
+        val adapter = QuestionAdapter { openLink(it) }
+        binding.recyclerView.adapter = adapter
+
+        viewModel.getQuestionsList().observe(this, { questionsList ->
+            questionsList?.let { adapter.updateList(it) }
+        })
+    }
+
+    private fun openLink(link: String) {
+        try {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
+            startActivity(intent)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 
 }
